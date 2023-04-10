8792251990b6ac6622a0d12d7ea61ca47a8f19dc
sylar-codex <vicaremy@gmail.com>
2023-04-10 22:46:25 +0100

    fixed home link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <nav>
-      <div>
+      <Link to="/" className="link">
         <h2>Skander Blaiti</h2>
-      </div>
+      </Link>
       <div>
         <ul className="space-x">
           <li>
